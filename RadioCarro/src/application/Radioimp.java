6608cb6556befc29dff3ef5d418996af12ad23fd
sorozcov <42949349+sorozcov@users.noreply.github.com>
2019-01-17 18:36:30 -0600

    radiooimp

---
+++ RadioCarro/src/application/Radioimp.java
@@ -0,0 +1,59 @@
+package application;
+
+public class Radioimp implements Radio {
+	
+	private 
+	private 
+	private
+	private 
+	private 
+	
+	@Override
+	public void toggle() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void changeFrequency() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void changeStation(boolean up) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getFrequency() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void saveStation(int numButton) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void changeStationButton(int numButton) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double getStation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
