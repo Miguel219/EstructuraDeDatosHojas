85470c0930323b808d989c7b3611bcf3a52a4d33
sorozcov <42949349+sorozcov@users.noreply.github.com>
2019-01-18 18:55:24 -0600

    tests

--- RadioCarro/src/application/MyTests.java
+++ RadioCarro/src/application/MyTests.java
@@ -1,5 +1,8 @@
 package application;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +13,17 @@ public class MyTests {
         Radio tester = new Radioimp(); // MyClass is tested
 
         // assert statements
+        //Aquí se ejecutara nuestro test.
         tester.toggle();
-        tester.toggle();
-        assertEquals(true, tester.getState(), "Estado inicial es apagado");
+        assertEquals(true, tester.getState(), "Se acaba de prender el radio. Debe estar encendido.");
+        tester.changeStation(true);
+        tester.changeStation(true);
+        tester.saveStation(3);
+        tester.changeFrequency();
+        tester.changeStationButton(3);
+        tester.getStation();
+        assertEquals(true, tester.getFrequency(), "Debe estar en esta frecuencia FM.");
+        assertEquals(0.3,88.3, tester.getStation(), "Debe estar en esta estacion que se guarda en el 3.");
         
     }
 }
